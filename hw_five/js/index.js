7a77b9a8e7f65e63981e2976d70aeebdf10262bb
John Swanson <john@swansonwriting.com>
2021-11-25 11:44:01 -0500

    HW5 Added buttons and getRandomLetter functionality

--- hw_five/js/index.js
+++ hw_five/js/index.js
@@ -1,31 +1,31 @@
-const data = {
-	"A": {"value":1,  "amount":9},
-	"B": {"value":3,  "amount":2},
-	"C": {"value":3,  "amount":2},
-	"D": {"value":2,  "amount":4},
-	"E": {"value":1,  "amount":12},
-	"F": {"value":4,  "amount":2},
-	"G": {"value":2,  "amount":3},
-	"H": {"value":4,  "amount":2},
-	"I": {"value":1,  "amount":9},
-	"J": {"value":8,  "amount":1},
-	"K": {"value":5,  "amount":1},
-	"L": {"value":1,  "amount":4},
-	"M": {"value":3,  "amount":2},
-	"N": {"value":1,  "amount":6},
-	"O": {"value":1,  "amount":8},
-	"P": {"value":3,  "amount":2},
-	"Q": {"value":10, "amount":1},
-	"R": {"value":1,  "amount":6},
-	"S": {"value":1,  "amount":4},
-	"T": {"value":1,  "amount":6},
-	"U": {"value":1,  "amount":4},
-	"V": {"value":4,  "amount":2},
-	"W": {"value":4,  "amount":2},
-	"X": {"value":8,  "amount":1},
-	"Y": {"value":4,  "amount":2},
-	"Z": {"value":10, "amount":1},
-	"_": {"value":0,  "amount":2}
+let letters = {
+	"A": {"value":1,  "amount":9,  "remaining":9},
+	"B": {"value":3,  "amount":2,  "remaining":2},
+	"C": {"value":3,  "amount":2,  "remaining":2},
+	"D": {"value":2,  "amount":4,  "remaining":4},
+	"E": {"value":1,  "amount":12,  "remaining":12},
+	"F": {"value":4,  "amount":2,  "remaining":2},
+	"G": {"value":2,  "amount":3,  "remaining":3},
+	"H": {"value":4,  "amount":2,  "remaining":2},
+	"I": {"value":1,  "amount":9,  "remaining":9},
+	"J": {"value":8,  "amount":1, "remaining":1},
+	"K": {"value":5,  "amount":1, "remaining":1},
+	"L": {"value":1,  "amount":4,  "remaining":4},
+	"M": {"value":3,  "amount":2,  "remaining":2},
+	"N": {"value":1,  "amount":6,  "remaining":6},
+	"O": {"value":1,  "amount":8,  "remaining":8},
+	"P": {"value":3,  "amount":2,  "remaining":2},
+	"Q": {"value":10, "amount":1, "remaining":1},
+	"R": {"value":1,  "amount":6,  "remaining":6},
+	"S": {"value":1,  "amount":4,  "remaining":4},
+	"T": {"value":1,  "amount":6,  "remaining":6},
+	"U": {"value":1,  "amount":4,  "remaining":4},
+	"V": {"value":4,  "amount":2,  "remaining":2},
+	"W": {"value":4,  "amount":2,  "remaining":2},
+	"X": {"value":8,  "amount":1, "remaining":1},
+	"Y": {"value":4,  "amount":2,  "remaining":2},
+	"Z": {"value":10, "amount":1, "remaining":1},
+	"_": {"value":0,  "amount":2,  "remaining":2}
 };
 let last;
 
@@ -42,14 +42,16 @@ $(document).ready(() => {
 	for(let i = ROW_SIZE; i < 2*ROW_SIZE; i ++) {
 		board.filledTiles.set(i, i - ROW_SIZE);
 	}
-	console.log(board.filledTiles);
+	console.log(getRandomLetter());
 });
 
+// Creates draggable tiles and locations to dorp them
 function initDragNDrop() {
 	// Add draggables & droppable locations to the screen
 	for(let i = 0; i < ROW_SIZE; i ++) {
+		const letter = getRandomLetter();
 		$('#topShelf').append("<div class='droppable' id="+i+"> </div>");
-		$('#botShelf').append("<div class='droppable' id="+(7+i)+"> <div class='draggable'> " + i + "</div> </div>");
+		$('#botShelf').append("<div class='droppable' id="+(7+i)+"> <div class='draggable'> " + letter[0]+ ", " + letter[1].value + "</div> </div>");
 	}
 
 
@@ -86,4 +88,13 @@ function initDragNDrop() {
 			}
 		}
 	});	
+}
+
+function getRandomLetter() {
+	const key = Object.keys(letters)[Math.floor(Math.random() * Object.keys(letters).length)];
+	if(letters[key].remaining > 0) {
+		letters[key].remaining --;
+		return [key, letters[key]]
+	}
+	return getRandomLetter();
 }
